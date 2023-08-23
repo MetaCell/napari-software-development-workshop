84b39a5c513e6aaa5105487013e293677168e950
Sean Martin <seankieran.m@hotmail.com>
2023-08-23 18:39:23 +0100

    Adding some intial widget fucntionality

--- src/napari_workshop_plugin/__init__.py
+++ src/napari_workshop_plugin/__init__.py
@@ -6,14 +6,15 @@ except ImportError:
 
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, example_magic_widget
+from ._widget import segmentation_widget
 from ._writer import write_multiple, write_single_image
 
+# this is the list of things that are imported with
+# from napari_workshop_plugin import *
 __all__ = (
     "napari_get_reader",
     "write_single_image",
     "write_multiple",
     "make_sample_data",
-    "ExampleQWidget",
-    "example_magic_widget",
+    "segmentation_widget"
 )
--- src/napari_workshop_plugin/_widget.py
+++ src/napari_workshop_plugin/_widget.py
@@ -1,46 +1,101 @@
 """
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
-
-Replace code below according to your needs.
+This module provides a widget for segmenting cells from calcium imaging data.
 """
 from typing import TYPE_CHECKING
 
+import numpy as np
 from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from napari.qt.threading import thread_worker
+from napari.utils.notifications import show_info
+from skimage import color, filters, morphology
 
 if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+@thread_worker
+def segment(
+    image: "napari.types.Image", blur_sigma: float = 2.0, disk_size: int = 4
+):
+    image_data = np.asarray(image.data)
+    image_data = blur_and_gray_image(image_data, blur_sigma)
+    yield (f"{image.name}_blurred", image_data, True)
+    labels = initial_segment(image_data)
+    yield (f"{image.name}_rough_segmented", labels, False)
+    labels = smooth_labels(labels, disk_size)
+    yield (f"{image.name}_segmented", labels, False)
 
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def blur_and_gray_image(
+    image: "napari.types.ImageData", sigma: float = 2.0
+) -> "napari.types.ImageData":
+    image = np.asarray(image)
+    if len(image.shape) >= 3 and image.shape[-1] in [3, 4]:
+        image = color.rgb2gray(image)
+    return filters.gaussian(image, sigma)
 
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def initial_segment(
+    image: "napari.types.ImageData", classes: int = 3
+) -> "napari.types.LabelsData":
+    image = np.asarray(image)
+    thresholds = filters.threshold_multiotsu(image, classes=classes)
+    return np.digitize(image, thresholds)
+
+
+def smooth_labels(
+    labels: "napari.types.LabelsData", disk_size: int = 4
+) -> "napari.types.LabelsData":
+    labels = np.asarray(labels)
+    return filters.rank.mean(labels, morphology.disk(disk_size))
+
+
+@magic_factory(persist=True)
+def segmentation_widget(
+    viewer: "napari.Viewer",
+    image: "napari.layers.Image",
+    blur_sigma: float = 2.0,
+    disk_size: int = 4,
+):
+    """
+    Segment cells from calcium image max projection.
+
+    1. Grayscale and blur the image.
+    2. Perform a multiotsu to separate the image into three classes:
+        background, non-cell tissue, and cells.
+    3. Smooth the multiotsu segmentation via disks.
+
+    Parameters
+    ----------
+    viewer : napari.Viewer
+        A viewer instance from napari
+    image : napari.layers.Image
+        The image to segment from. Can be one channel or more.
+        If more than one channel, is converted to grayscale.
+    blur_sigma : float, optional
+        The standard deviation of the gaussian blur to be applied.
+        A higher sigma indicates more blur.
+        By default 2.0.
+    disk_size : int, optional
+        The size of the disk in pixels for smoothing.
+        A higher disk_size indicates more smoothing.
+        By default 4.
+
+    """
+
+    def display_layer(args) -> "napari.types.LayerDataTuple":
+        name, data, as_image = args
+        if name in viewer.layers:
+            show_info(f"Updating {name} with new data")
+            viewer.layers[name].data = data
+        else:
+            show_info(f"Adding {name} to viewer")
+            (
+                viewer.add_image(data, name=name)
+                if as_image
+                else viewer.add_labels(data, name=name)
+            )
+
+    worker = segment(image, blur_sigma, disk_size)
+    worker.yielded.connect(display_layer)
+    worker.start()
