27b1a53626a096880660b51e221a092c867a82e5
Sean Martin <seankieran.m@hotmail.com>
2023-09-05 15:38:45 +0100

    Formatting

--- src/napari_workshop_plugin/__init__.py
+++ src/napari_workshop_plugin/__init__.py
@@ -1,16 +1,11 @@
-
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
 
-from ._reader import read_numpy_file, napari_get_reader
+from ._reader import napari_get_reader, read_numpy_file
 from ._widget import segmentation_widget
 
 # this is the list of things that are imported with
 # from napari_workshop_plugin import *
-__all__ = (
-    "segmentation_widget"
-    "read_numpy_file",
-    "napari_get_reader"
-)
+__all__ = ("segmentation_widget" "read_numpy_file", "napari_get_reader")
