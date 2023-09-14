c2e8eee9f58cdc009590e6ecce25614a647d11a7
Simão sá <simao@metacell.us>
2023-09-14 12:37:07 +0100

    added more reader tests

--- src/napari_workshop_plugin/_tests/test_reader.py
+++ src/napari_workshop_plugin/_tests/test_reader.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from napari_workshop_plugin import napari_get_reader
+from napari_workshop_plugin import napari_get_reader, read_numpy_file
 
 
 # tmp_path is a pytest fixture
@@ -28,3 +28,52 @@ def test_reader(tmp_path):
 def test_get_reader_pass():
     reader = napari_get_reader("fake.file")
     assert reader is None
+
+
+def test_napari_get_reader_single_file():
+    # Test the napari_get_reader function with a single file path.
+    path = "test.npy"
+    reader = napari_get_reader(path)
+    assert callable(reader)
+
+def test_napari_get_reader_multiple_files():
+    # Test the napari_get_reader function with a list of file paths.
+    paths = ["test1.npy", "test2.npy"]
+    reader = napari_get_reader(paths)
+    assert callable(reader)
+
+def test_napari_get_reader_non_npy():
+    # Test the napari_get_reader function with a non-npy file path.
+    path = "test.txt"
+    reader = napari_get_reader(path)
+    # assert callable(reader) # to make it fail
+    assert reader is None
+
+def test_read_numpy_file_single_file(tmp_path):
+    # Test the read_numpy_file function with a single npy file.
+    path = str(tmp_path / "test.npy")
+    original_data = np.random.rand(20, 20)
+    np.save(path, original_data)
+    layer_data = read_numpy_file(path)
+    assert isinstance(layer_data, list)
+    assert len(layer_data) == 1
+    data, add_kwargs, layer_type = layer_data[0]
+    assert isinstance(data, np.ndarray)
+    assert data.shape == original_data.shape
+    np.testing.assert_allclose(data, original_data)
+
+def test_read_numpy_file_multiple_files(tmp_path):
+    # Test the read_numpy_file function with multiple npy files.
+    paths = [str(tmp_path / "test1.npy"), str(tmp_path / "test2.npy")]
+    original_data1 = np.random.rand(20, 20)
+    original_data2 = np.random.rand(20, 20)
+    np.save(paths[0], original_data1)
+    np.save(paths[1], original_data2)
+    layer_data = read_numpy_file(paths)
+    assert isinstance(layer_data, list)
+    assert len(layer_data) == 1
+    data, add_kwargs, layer_type = layer_data[0]
+    assert isinstance(data, np.ndarray)
+    assert data.shape == (2, 20, 20)
+    np.testing.assert_allclose(data[0], original_data1)
+    np.testing.assert_allclose(data[1], original_data2)
